0483502ee8d04af4be46c59e72746cd0daec1f9b
Nathanial Johnson <johnsonn@nathanials-mbp.lan>
2021-04-02 15:01:23 -0500

    added menu reroll functionality. added function to ensure reroll doesn't return the same result.

--- daytripGenerator.js
+++ daytripGenerator.js
@@ -6,9 +6,37 @@ let restaurantArray = ["Zombie Burger", "Ika", "Golden Bowl", "iPho#1","Sinful B
 let transportationArray = ["car", "bus", "bike", "taxi", "giraffe"];
 let entertainmentArray = ["see a concert", "play Frisbee", "fly a kite", "play yard games", "be the only person in attendance for a bad mime's first performance"];
 
+//Have to instantiate them first in order for ensureNewResult function to work
+let destination;
+let restaurant;
+let transportation;
+let entertainment;
+
+destination = selectRandom(destinationArray);
+restaurant = selectRandom(restaurantArray);
+transportation = selectRandom(transportationArray);
+entertainment = selectRandom(entertainmentArray);
+
+let optionString = "Does this sound like a good plan?\nTo change the destination enter '1'.\nTo change the restaurant enter '2'.\n" +
+"To change the mode of transportation enter '3'.\nTo change the entertainment enter '4'.\nIf you'd like to get a whole new day enter '5'.\n" +
+"If you are happy with this plan enter '6'";
+
+function resultString(transportation, destination, restaurant, entertainment){
+    return "Your totally random day will be taking a " + transportation + " to the " + destination 
+    + " area. While there eat at " + restaurant + ", and for entertainment you can " + entertainment + "!";
+}
+
+function ensureNewResult(int, length){
+    if(destinationArray[int] == destination || restaurantArray[int] == restaurant || transportationArray[int] == transportation || entertainmentArray[int] == entertainment){
+        int = getRandomInt(length);
+    }
+    return int;
+}
 //random number generator
 function getRandomInt(length){
-    return Math.floor(Math.random() * length);
+    let int = Math.floor(Math.random() * length);
+    int = ensureNewResult(int, length);
+    return int;
 }
 
 function selectRandom(array){
@@ -16,12 +44,36 @@ function selectRandom(array){
     return result;
 }
 
-let destination = selectRandom(destinationArray);
-let restaurant = selectRandom(restaurantArray);
-let transportation = selectRandom(transportationArray);
-let entertainment = selectRandom(entertainmentArray);
+console.log("Thanks " + userName + "! " + resultString(transportation, destination, restaurant, entertainment));
 
+let result = prompt(optionString);
+// Menu
+while(result != 6){
+    switch(result){
+        case "1":
+            destination = selectRandom(destinationArray);
+            break;
+        case "2":
+            restaurant = selectRandom(restaurantArray);
+            break;
+        case "3":
+            transportation = selectRandom(transportationArray);
+            break;
+        case "4":
+            entertainment = selectRandom(entertainmentArray);
+            break;
+        case "5":
+            destination = selectRandom(destinationArray);
+            restaurant = selectRandom(restaurantArray);
+            transportation = selectRandom(transportationArray);
+            entertainment = selectRandom(entertainmentArray);
+            break;
+        default:
+            result = alert("We need a number 1-6");
+            break;
+    }
+    console.log(resultString(transportation, destination, restaurant, entertainment));
+    result = prompt(optionString);
+}
 
-console.log("Thanks " + userName + "! Your totally random day will be taking a " + transportation + " to the " + destination 
-+ " area. While there eat at " + restaurant + ", and for entertainment you can " + entertainment + "!");
-
+console.log("Have a great time!");
