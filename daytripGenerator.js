c13e69c35c36fc8954f696618456ad782d2a8e94
Nathanial Johnson <johnsonn@nathanials-mbp.lan>
2021-04-02 14:02:25 -0500

    Added random functionality

--- daytripGenerator.js
+++ daytripGenerator.js
@@ -1,11 +1,27 @@
 "use strict"
 
 let userName = prompt("Hello! Welcome to the Daytrip Generator. Give me you name and I'll plan you a day!");
-let destination = ["East Village", "Benson", "Old Market", "Aksarben", "Valley Junction", "Jordan Creek"];
-let restaurant = ["Zombie Burger", "Ika", "Golden Bowl", "iPho#1","Sinful Burger", "Tasty Tacos", "Cactus Bobs"];
-let transportation = ["car", "bus", "bike", "taxi", "giraffe"];
-let entertainment = ["see a concert", "play Frisbee", "fly a kite", "play yard games", "be the only person in attendance for a bad mime's first performance"];
+let destinationArray = ["East Village", "Benson", "Old Market", "Aksarben", "Valley Junction", "Jordan Creek"];
+let restaurantArray = ["Zombie Burger", "Ika", "Golden Bowl", "iPho#1","Sinful Burger", "Tasty Tacos", "Cactus Bobs"];
+let transportationArray = ["car", "bus", "bike", "taxi", "giraffe"];
+let entertainmentArray = ["see a concert", "play Frisbee", "fly a kite", "play yard games", "be the only person in attendance for a bad mime's first performance"];
 
-console.log("Thanks " + userName + "! Your totally random day will be taking a " + transportation[0] + " to the " + destination[0] 
-+ " area. While there eat at " + restaurant[0] + ", and for entertainment you can " + entertainment[0] + "!");
+//random number generator
+function getRandomInt(length){
+    return Math.floor(Math.random() * length);
+}
+
+function selectRandom(array){
+    let result = array[getRandomInt(array.length)];
+    return result;
+}
+
+let destination = selectRandom(destinationArray);
+let restaurant = selectRandom(restaurantArray);
+let transportation = selectRandom(transportationArray);
+let entertainment = selectRandom(entertainmentArray);
+
+
+console.log("Thanks " + userName + "! Your totally random day will be taking a " + transportation + " to the " + destination 
++ " area. While there eat at " + restaurant + ", and for entertainment you can " + entertainment + "!");
 
